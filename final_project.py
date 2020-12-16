248cc4b82c0f14321f904efa3d977c0817724269
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-15 20:17:15 -0800

    add changes

---
+++ final_project.py
@@ -0,0 +1,7 @@
+"""
+This program is trying to determine a person's weekly pay
+he or she receives time and a half for overtime work beyond 40 hours
+The main function should call 3 function for input 
+Then calculate the value
+Calculate the ouput 
+"""
