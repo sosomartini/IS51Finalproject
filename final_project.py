a1e93006489bbba47a8069d2e0e62a488dfe4423
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-15 20:22:21 -0800

    pseudo-code

--- final_project.py
+++ final_project.py
@@ -5,3 +5,24 @@ The main function should call 3 function for input
 Then calculate the value
 Calculate the ouput 
 """
+
+"""
+Get the wages and hours rate = input()
+determine pay for the week = pay(wages)
+display earnings
+
+Get the wages and hours = input()
+Worked hours = input()
+hourly wage = input()
+
+
+Pay()  # calculate weekly pay with time and a half
+    if hours is greater than or equal 40
+        amount is wages times hours
+    else:
+        amount equals wages times 40 + (1.5) times wages times (hours-40)
+
+display earnings
+    Print()
+
+"""
